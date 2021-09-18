9e7519f794d45cb4c312b4323e84764c4d2b1ed6
9382 <omnomaidan@gmail.com>
2021-09-18 22:49:08 +0100

    Fix File Creation
    python

--- PyTTS.py
+++ PyTTS.py
@@ -3,5 +3,5 @@ import pyttsx3
 ttsObject = pyttsx3.init()
 ttsObject.setProperty("voice",ttsObject.getProperty('voices')[0].id)
 ttsObject.setProperty("rate",160)
-ttsObject.save_to_file(text,"TTS.mp3")
+ttsObject.save_to_file(text,"plugins/TTS.mp3")
 ttsObject.runAndWait()
